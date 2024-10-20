b54264f18dae8f55248fff02e8b8beb6fa735b3b
SokWith <129967025+SokWith@users.noreply.github.com>
2024-10-20 14:44:09 +0800

    Update _middleware.js

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -308,23 +308,21 @@ async function handleRequest(request, env,ctx) {
         }
 
 if (resUrl.pathname == "/c/api/start") {
-  retBody = {
-    ...retBody,
-    "features": [
-      "humanchallenge",
-      "clarity",
-      "csamplevariant",
-      "aaflight_t",
-      "daily-briefing",
-      "onboarding",
-      "gndsnippet3500",
-      "cf-turnstile",
-      "stopexp",
-      "dailybriefing",
-      "upload-image"
-    ]
-  };
+  retBody.features = [
+    "humanchallenge",
+    "clarity",
+    "csamplevariant",
+    "aaflight_t",
+    "daily-briefing",
+    "onboarding",
+    "gndsnippet3500",
+    "cf-turnstile",
+    "stopexp",
+    "dailybriefing",
+    "upload-image"
+  ];
 }
+
 
         
         if (resUrl.pathname == "/images/create" || resUrl.pathname.startsWith("/images/create/") && !resUrl.pathname.startsWith("/images/create/async/")) {
